84dd2b5fcf5bd59cc0401d6cc8736bd4f970df8b
mhd8a <139964315+mhd8a@users.noreply.github.com>
2023-12-13 10:21:50 +0530

    Delete flask-password-hashing-bcrypt directory

--- flask-password-hashing-bcrypt/test.py
+++
@@ -1,6 +0,0 @@
-from bcrypt import hashpw, checkpw, gensalt
-
-hashed_password = hashpw('foobar'.encode('utf-8'), gensalt())
-
-print(checkpw('foobar'.encode('utf-8'), hashed_password)) # prints True
-print(checkpw('foobarbaz'.encode('utf-8'), hashed_password)) # prints False
